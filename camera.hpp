18f228c69bfcfdbf7924605686d77b11db316faa
Kenneth Strieker <reidstrieker869@gmail.com>
2022-03-11 23:18:40 -0700

    adding comments for further clarity

--- camera.hpp
+++ camera.hpp
@@ -19,25 +19,25 @@ class camera {
         mVector<double> getUpDirection(); // return up vector
         mVector<double> getU(); // get local horizontal screen coordinate vector
         mVector<double> getV(); // get local vertical screen coordinate vector
-        mVector<double> getScreenCenter();
-        double getLength();
-        double getHorizontal();
-        double getAspectRatio();
-        ray createRay(float proScreenX, float proScreenY);
-        void updateCameraGeometry();
+        mVector<double> getScreenCenter(); // return vector to center of screen
+        double getLength(); // return distance of projection screen from pinhole
+        double getHorizontal(); // return horizontal pixel count
+        double getAspectRatio(); // return aspect ratio
+        ray createRay(float proScreenX, float proScreenY); // function to create a light ray
+        void calculateGeometry(); // function to calculate camera geometry vectors
 
     private:
         // VARIABLES:
-        mVector<double> position{3};
-        mVector<double> frame{3};
-        mVector<double> upDir{3};
-        double length;
-        double horizontal;
-        double aspectRatio;
+        mVector<double> position{3}; // camera position
+        mVector<double> frame{3}; // direction for camera to look at
+        mVector<double> upDir{3}; // up direction vector
+        double length; // distance of projection screen from pinhole
+        double horizontal; // horizontal screen pixel count
+        double aspectRatio; // image aspect ratio
         mVector<double> alignmentVector{3};
-        mVector<double> projectionScreenU{3};
-        mVector<double> projectionScreenV{3};
-        mVector<double> projectionScreenCenter{3};
+        mVector<double> projectionScreenU{3}; // local horizontal screen coordinate vector
+        mVector<double> projectionScreenV{3}; // local vertical screen coordinate vector
+        mVector<double> projectionScreenCenter{3}; // screen center
 };
 
 #endif
